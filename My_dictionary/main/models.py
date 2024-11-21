a0b5d52418c955801b664d713a88367af007bfb6
MEGANIS <->
2024-11-21 16:25:29 +0300

    Add unique parametrs for word and meaning Commented* sort by asc

--- My_dictionary/main/models.py
+++ My_dictionary/main/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Word(models.Model):
-    word = models.CharField(max_length=50)
-    meaning = models.TextField()
+    word = models.CharField(max_length=50, blank=False, unique=True)
+    meaning = models.TextField(max_length=50, blank=False, unique=True)
 
     def __str__(self):
         return self.word
